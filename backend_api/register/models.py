838d461bd3ba92cf433f0421c663d516ecd3d908
Ivan Kuznetsov <Ivan.kuzznetsov@gmail.com>
2024-06-05 22:41:53 +0500

    add frontend, backend_api, cors

---
+++ backend_api/register/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Users(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
