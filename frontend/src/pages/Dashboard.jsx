f9a497862e97c60df2176d6d8b71a0af290027a8
Pavel M <merekin33@gmail.com>
2024-06-18 14:09:42 +0800

    dashboard changed

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,12 @@
+import Substrate from "../components/UI/Substrate";
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Substrate width="300px">
+      <h3>Имя пользователя: </h3>
+      <h3>Почта: </h3>
+    </Substrate>
+  );
 };
 
 export default Dashboard;
